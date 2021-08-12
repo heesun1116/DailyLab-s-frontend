dfcd93d8d255a83337c088a8429ec50f473d9b73
heesun@gamil.com <70732062+heesun1116@users.noreply.github.com>
2021-08-12 18:26:55 +0900

    deploy

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const client = axios.create({ baseURL: 'https://daily-labs.herokuapp.com/' });
+const client = axios.create();
 
 // client.default.baseURL = 'https://daily-labs.herokuapp.com/';
 export default client;
